5d38b36b6b7a082cf0ce4ef34dc3cdeef0cda74b
Chamali Vishmani <chamali106astro@gmail.com>
2024-03-08 17:29:45 +0530

    create embedding from local pdf

--- askBook/makeEmbeddings.py
+++ askBook/makeEmbeddings.py
@@ -2,10 +2,9 @@ import pypyodbc
 import pickle
 
 from langchain_community.llms import Ollama
-from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
@@ -31,16 +30,13 @@ def databaseConncetion():
 
 def createVectorAndAddToDB(cursor, conn):
     # create vector
-    llm = Ollama(model="llama2")
-    lease_note_loader = WebBaseLoader(
-        "https://drive.google.com/file/d/1grZyR7sXYPY0eiSqh_Yr5-99xPZ7snL_/view?usp=drive_link")
-    finance_docs = lease_note_loader.load()
-
+    lease_note_loader = PyPDFLoader("pdfs/03_Leases.pdf")
+    finance_pages = lease_note_loader.load_and_split()
+    print("finance_docs ", finance_pages[3])
     embeddings = OllamaEmbeddings()
 
-    text_splitter = RecursiveCharacterTextSplitter()
-    documents = text_splitter.split_documents(finance_docs)
-    vector = FAISS.from_documents(documents, embeddings)
+    vector = FAISS.from_documents(finance_pages, embeddings)
+    print("vector ", vector)
 
     # parameters to add to the database
     vector_serialized = pickle.dumps(vector)
@@ -51,34 +47,43 @@ def createVectorAndAddToDB(cursor, conn):
     cursor.execute(query)
     conn.commit()
 
-    # add_vector_query = """INSERT INTO embeddings (document_id, embedding) VALUES (?, ?)"""
     add_vector_query = """
     MERGE INTO embeddings AS target
     USING (VALUES (?, ?)) AS source (document_id, embedding)
     ON target.document_id = source.document_id
+    WHEN MATCHED THEN
+        UPDATE SET embedding = source.embedding
     WHEN NOT MATCHED THEN
         INSERT (document_id, embedding) VALUES (?,?);
-
     """
     cursor.execute(add_vector_query, (document_id,
                                       vector_serialized, document_id, vector_serialized))
     conn.commit()
 
+    return vector
 
-def askQuestion():
-    pass
+
+def askQuestion(vector):
+    llm = Ollama(model="llama2")
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based on the provided context:
+    <context>
+    {context}
+    <context>
+
+    Question: {input}""")
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+
+    retriever = vector.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    response = retrieval_chain.invoke(
+        {"input": "What is a lease and what are the two types of leases?"})
+    print(response)
 
 
 if __name__ == "__main__":
+
     cursor, conn = databaseConncetion()
-    createVectorAndAddToDB(cursor, conn)
 
-
-# prompt = ChatPromptTemplate.from_template("""Answer the following question based on the provided context:
-#     <context>
-#     {context}
-#     <context>
-
-#     Question: {input}""")
-
-# document_chain = create_stuff_documents_chain(llm, prompt)
+    vector = createVectorAndAddToDB(cursor, conn)
+    askQuestion(vector)
